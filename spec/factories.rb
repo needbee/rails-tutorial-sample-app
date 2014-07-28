278a588a8c7644bd9639f32e4c968c42347acd0f
Josh Justice <josh@need-bee.com>
2014-07-28 06:33:23 -0400

    adds micro posts; Squashed commit of the following:
    commit 53044a8f021eb9c7676c2cd97d0e7def0c129c2a
    Author: Josh Justice <josh@need-bee.com>
    Date:   Mon Jul 28 06:32:29 2014 -0400
    
        adds ability to delete micro post
    
    commit 74789b89b0472e5f72565ea8bc2fe64e83d19b7e
    Author: Josh Justice <josh@need-bee.com>
    Date:   Sun Jul 27 07:44:50 2014 -0400
    
        adds micro post feed
    
    commit 6a6c6ac5a08d76a3243ced3fc3b5816107cf320b
    Author: Josh Justice <josh@need-bee.com>
    Date:   Sun Jul 27 07:34:53 2014 -0400
    
        adds ability to add a post
    
    commit 37bb6925803ba455d05c60058675eb0f4f5715e3
    Author: Josh Justice <josh@need-bee.com>
    Date:   Sat Jul 26 14:06:01 2014 -0400
    
        protects mp create/delete methods
    
    commit d5eab0bf5f7420e6e0afef37c42877308b31db3c
    Author: Josh Justice <josh@need-bee.com>
    Date:   Sat Jul 26 13:58:08 2014 -0400
    
        adds micro post sample data
    
    commit ecef06e70174852c8d6bc5e74c347b2e2f4dc93d
    Author: Josh Justice <josh@need-bee.com>
    Date:   Sat Jul 26 13:55:41 2014 -0400
    
        adds micro posts to user page
    
    commit f673bfe075770d3f16c934937aff121d7e2b65a7
    Author: Josh Justice <josh@need-bee.com>
    Date:   Sat Jul 26 11:09:24 2014 -0400
    
        adds validations
    
    commit 0136177eced49cb52c56cda19870b6ca593be09f
    Author: Josh Justice <josh@need-bee.com>
    Date:   Sat Jul 26 11:07:49 2014 -0400
    
        adds delete cascade to micropost
    
    commit b2972fa2942d8fc6c4ffc26086665c8d5ec64819
    Author: Josh Justice <josh@need-bee.com>
    Date:   Sat Jul 26 10:41:13 2014 -0400
    
        adds default sort order
    
    commit 96a9c8aecc6336bda77cfefaf46abca8f42146f9
    Author: Josh Justice <josh@need-bee.com>
    Date:   Sat Jul 26 10:36:29 2014 -0400
    
        adds micropost/user relationship
    
    commit 377c33a64d21e2acf68733a5df46ad72af3396b0
    Author: Josh Justice <josh@need-bee.com>
    Date:   Sat Jul 26 10:34:32 2014 -0400
    
        adds micropost

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,11 +2,16 @@ FactoryGirl.define do
   factory :user do
     sequence(:name)   { |n| "Person #{n}" }
     sequence(:email)  { |n| "person_#{n}@example.com"}
-    password  "password"
+    password "password"
     password_confirmation "password"
 
     factory :admin do
       admin true
     end
+  end
+
+  factory :micropost do
+    content 'Lorem ipsum'
+    user
   end
 end
